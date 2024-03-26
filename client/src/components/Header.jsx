baabbcd3fe896b312155d7fd3d3b4883e0dcc891
udara88 <udarawickramasinghe88@gmail.com>
2024-03-26 09:13:42 +0530

    style header section

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,27 @@
-import {Navbar} from 'flowbite-react'
+import {FaSearch} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
 
 const Header = () => {
   return (
-   <Navbar>Header</Navbar>
+   <header className='bg-slate-200'>
+    <nav className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+      <span className='text-slate-500'>Sahand  </span> 
+      <span className='text-slate-700'> Estate</span>
+     </h1>
+     
+     <form className='flex items-center  bg-slate-100 rounded-lg p-2'>
+      <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none border-none w-24 sm:w-64 '/>
+      <FaSearch/>
+     </form>
+     <ul className='sm:flex gap-4  hidden'>
+      <Link to='/' className='hidden sm:inline text-slate-700 hover:underline'>Home</Link>
+      <Link to='/about' className='hidden sm:inline text-slate-700 hover:underline'>About</Link>
+      <Link to='/sign-in' className='hidden sm:inline text-slate-700 hover:underline'>Sign in</Link>
+     </ul>
+    </nav>
+   </header>
   )
 }
 
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,17 +1,15 @@
 import Home from "./Home"
 import About from "./About"
 import Profile from "./Profile"
-import Projects from "./Projects"
 import Signin from "./Signin"
 import SignUp from "./SignUp"
-import Dashboard from "./Dashboard"
+
 
 export {
     Home,
     About,
     Profile,
-    Projects,
     Signin,
     SignUp,
-    Dashboard
+    
 }
