bed6efc3da3358dd342ae7ca7a7ce0ff1c22c2f5
udara88 <udarawickramasinghe88@gmail.com>
2024-03-26 08:00:38 +0530

    add pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Home,About,Signin,SignUp,Dashboard,Projects} from './pages/'
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
+      <Route path='/sign-in' element={<Signin/>}></Route>
+      <Route path='/sign-up' element={<SignUp/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      <Route path='/projects' element={<Projects/>}></Route>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home"
+import About from "./About"
+import Profile from "./Profile"
+import Projects from "./Projects"
+import Signin from "./Signin"
+import SignUp from "./SignUp"
+import Dashboard from "./Dashboard"
+
+export {
+    Home,
+    About,
+    Profile,
+    Projects,
+    Signin,
+    SignUp,
+    Dashboard
+}
