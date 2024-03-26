24a773a8cc78ab6b44153eda94bcf9bb75693e21
udara88 <udarawickramasinghe88@gmail.com>
2024-03-26 08:41:16 +0530

    create route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import {Home,About,Signin,SignUp,Dashboard,Projects} from './pages/'
+import {Home,About,Signin,SignUp,Profile} from './pages/'
+import Header from './components/Header'
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
      <Routes>
       <Route path='/' element={<Home/>}></Route>
       <Route path='/about' element={<About/>}></Route>
       <Route path='/sign-in' element={<Signin/>}></Route>
       <Route path='/sign-up' element={<SignUp/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
-      <Route path='/projects' element={<Projects/>}></Route>
+      <Route path='/profile' element={<Profile/>}></Route>
+     
      </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import {Navbar} from 'flowbite-react'
+import {Link} from 'react-router-dom'
+
+
+const Header = () => {
+  return (
+   <Navbar>Header</Navbar>
+  )
+}
+
+export default Header
