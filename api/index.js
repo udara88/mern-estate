5703aa4e86a740a78c90be697155ccc88ceafe76
udara88 <udarawickramasinghe88@gmail.com>
2024-03-26 10:11:49 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_URL).then(()=>{
+    console.log('Connected to database')
+}).catch((err)=>{
+    console.log(err)
+})
+
 const app = express()
 
 app.listen(3000,()=>{
